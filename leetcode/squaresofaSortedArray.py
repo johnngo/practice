24da1fb9caf95220085eb297138cd5f9dae40701
JJ <ngongo_jn@hotmail.com>
2022-06-09 20:49:12 -0700

    squares of a sorted array

---
+++ leetcode/squaresofaSortedArray.py
@@ -0,0 +1,18 @@
+#977 Squares of a Sorted Array
+##Given an integer array nums sorted in non-decreasing
+##order return an array of the squares of each number
+##sorted in non-decreasing order
+from typing import List
+
+def sortedSquares(nums: List[int]) -> List[int]:
+  result = []
+  left, right = 0, len(nums) - 1
+  for index in range(len(nums)-1, -1, -1):
+    if abs(nums[left] > abs(nums[right])):
+      result.append(nums[left] ** 2)
+      left += 1
+    else:
+      result.append(nums[right] ** 2)
+      right -= 1
+    result.reverse()
+    return result
