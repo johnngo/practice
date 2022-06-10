7a5d0c4c5052d79f2808efbed870be3c7aff6fc0
JJ <ngongo_jn@hotmail.com>
2022-06-09 21:58:29 -0700

    RotateArray

---
+++ leetcode/RotateArray.py
@@ -0,0 +1,30 @@
+##Given an array, rotate the array to the right by "k"
+##steps, where k is non-negative
+
+def reverse(nums, i, j):
+  li = i
+  ri = j
+
+  while li < ri:
+    temp = nums[li]
+    nums[li] = nums[ri]
+    nums[ri] = temp
+
+    li +=1
+    ri -=1
+
+def rotate(nums, k: int) -> None:
+  k = k % len(nums)
+  if k < 0:
+    k +=len(nums)
+  
+  reverse(nums, 0, len(nums) - k - 1)
+  reverse(nums, len(nums) - k, len(nums) - 1)
+  reverse(nums, 0, len(nums) - 1)
+  
+#Input: nums = [1,2,3,4,5,6,7], k = 3
+# #Output: [5,6,7,1,2,3,4]
+#Explanation:
+#rotate 1 steps to the right: [7,1,2,3,4,5,6]
+#rotate 2 steps to the right: [6,7,1,2,3,4,5]
+#rotate 3 steps to the right: [5,6,7,1,2,3,4]
