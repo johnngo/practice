7220ca213ca34206b2df5de453b257cbe852cbd2
JJ <ngongo_jn@hotmail.com>
2022-06-13 15:48:32 -0700

    add binary search approach

--- leetcode/TwoSumInputArrayIsSorted.py
+++ leetcode/TwoSumInputArrayIsSorted.py
@@ -1,5 +1,4 @@
 #Two Pointer Strategy
-
 def twoSum(numbers, target):
   l, r = 0, len(numbers) - 1
   while l < r:
@@ -25,5 +24,18 @@ def twoSum(numbers, target):
       return [dic[target-num]+1, i + 1]
     dic[num] = i
 
-    
+##Binary Search
+##function with for loop, then while loop
+def twoSum(numbers,target):
+  for i in range(len(numbers)):
+    l, r = i+1, len(numbers) - 1
+    tmp = target - numbers[i]
+    while l <= r:
+      mid = l + (r-l)//2
+      if numbers[mid] == tmp:
+        return [i+1, mid+1]
+      elif numbers[mid] < tmp:
+        l = mid + 1
+      else:
+        r =mid - 1 
 
