1351dee7c4671db9538ec5d0d69c5ee72031275f
JJ <ngongo_jn@hotmail.com>
2022-06-13 17:28:10 -0700

    add reverse words in a string

---
+++ leetcode/ReverseWordsInAString.py
@@ -0,0 +1,20 @@
+#reverse Words
+
+def reverseWords(self, s: str) -> str:
+        left, right = 0, 0
+        res = []
+        while right < len(s):
+            if s[right] == ' ':
+                cur_word = s[left:right]
+                res.append(cur_word[::-1])
+                left = right + 1
+        
+            right += 1
+        
+        res.append(s[left:right][::-1])
+        
+        return " ".join(res)
+
+#Input = "Let's take LeetCode contest"
+#output = "s'teL ekat edoCteeL tsetnoc"
+
