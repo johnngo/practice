78dbcd33dcd78fcff834ea32eba9ff967c569f62
JJ <ngongo_jn@hotmail.com>
2022-06-14 21:05:41 -0700

    find length of the longest substring without repeating characters

---
+++ leetcode/LongestSubstringWithoutRepeatingCharacters.py
@@ -0,0 +1,19 @@
+## given a string s, find the length of the longest substring
+## without repeating characters
+
+##Sliding window approach
+
+
+def lengthOfLongestSubstring(s):
+  start = maxLength = 0
+  usedChar = {}
+
+  for i in range(len(s)):
+    if s[i] in usedChar and start <=usedChar[s[i]]:
+      start = usedChar[s[i]] + 1
+    else:
+      maxLength = max(maxLength, i - start + 1)
+
+    usedChar[s[i]] = I
+
+    return maxLength
